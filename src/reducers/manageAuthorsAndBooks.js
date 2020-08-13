2271d076b815e357f18f744c1a01d36ac094e4da
imperfectcastle <bart.sakowski@gmail.com>
2020-08-12 20:44:06 -0500

    Done.

--- src/reducers/manageAuthorsAndBooks.js
+++ src/reducers/manageAuthorsAndBooks.js
@@ -1,7 +1,6 @@
 import { combineReducers } from 'redux';
 import uuid from "uuid";
 
-
 const rootReducer = combineReducers({
   authors: authorsReducer,
   books: booksReducer
@@ -24,7 +23,6 @@ function booksReducer(state = [], action) {
   }
 }
 
-
 function authorsReducer(state = [], action) {
   let idx;
   switch (action.type) {
@@ -44,50 +42,7 @@ function authorsReducer(state = [], action) {
       } else {
         return [...state, { authorName: action.book.authorName, id: uuid() }];
       }
-
     default: 
       return state;  
   }
 }
-
-
-
-// export default function bookApp(
-//   state = {
-//     authors: [],
-//     books: []
-//   },
-//   action
-// ) {
-//   let idx;
-//   switch (action.type) {
-//     case "ADD_BOOK":
-//       return {
-//         ...state,
-//         books: [...state.books, action.book]
-//       };
-
-//     case "REMOVE_BOOK":
-//       idx = state.books.findIndex(book => book.id === action.id);
-//       return {
-//         ...state,
-//         books: [...state.books.slice(0, idx), ...state.books.slice(idx + 1)]
-//       };
-
-//     case "ADD_AUTHOR":
-//       return {
-//         ...state,
-//         authors: [...state.authors, action.author]
-//       };
-
-//     case "REMOVE_AUTHOR":
-//       idx = state.authors.findIndex(author => author.id === action.id);
-//       return {
-//         ...state,
-//         authors: [...state.authors.slice(0, idx), ...state.authors.slice(idx + 1)]
-//       };
-
-//     default:
-//       return state;
-//   }
-// }
